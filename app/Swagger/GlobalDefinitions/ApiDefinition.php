044e09fcdf6521b7d943696266831b84b128bffc
midesweb <malvarez@desarrolloweb.com>
2024-08-30 10:06:36 -0300

    instalar swagger ui

---
+++ app/Swagger/GlobalDefinitions/ApiDefinition.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Swagger\GlobalDefinitions;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *  title="Todo Rest API", 
+ *  version="0.0",
+ *  description="Este API permite implementar un sencillo sistema de 'To do list' con fines didácticos.",
+ *  @OA\Contact(
+ *    name="Soporte API",
+ *    url="https://escuela.it/contacto",
+ *    email="contacto@escuela.it"
+ *  ),
+ *  @OA\License(
+ *    name="MIT",
+ *    url="https://opensource.org/license/mit/"
+ *  )
+ * )
+ */
+
+ class ApiDefinition {
+  //
+ }
