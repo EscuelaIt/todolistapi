c8bc45e01d1602f552a0a6a26c57bbf458cdbc0e
midesweb <malvarez@desarrolloweb.com>
2024-08-30 18:33:42 -0300

    enlace a github

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,6 +40,11 @@
                                 <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="#66dddd"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
                                 <a href="/api/documentation">Documentación de los endpoints del API</a>
                             </li>
+                            <li class="flex items-center">
+                                <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="#66dddd"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
+                                <a href="https://github.com/EscuelaIt/todolistapi">Repositorio con el proyecto de API en GitHub</a>
+                            </li>
+                            
                         </ul>
                 </dile-card>
 
