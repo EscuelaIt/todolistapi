7c977369200b33a9deddb762109d203bc2452a82
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2024-09-25 04:36:42 +0200

    cambiar el welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>API de gestión del tiempo</title>
+        <title>API de todo list</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
